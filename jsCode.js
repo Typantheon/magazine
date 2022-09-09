88a568aecfeeb6929faf20dd56ac73326946ccf6
Sebastiano-G <92300303+Sebastiano-G@users.noreply.github.com>
2022-09-09 09:45:04 +0200

    Update jsCode.js

--- jsCode.js
+++ jsCode.js
@@ -43,6 +43,7 @@ function addStylesheet(fileName) {
 };
 
 function highlight(entity) {
+    console.log(entity); 
     const spans = document.getElementsByTagName("span");
     for (let i = 0; i < spans.length; i++) {
       if (spans[i].getAttribute('place') == entity) {
