b0c547346cb042c345b572582154abcdbf168050
Sebastiano-G <92300303+Sebastiano-G@users.noreply.github.com>
2022-09-10 00:20:31 +0200

    Update jsCode.js

--- jsCode.js
+++ jsCode.js
@@ -64,7 +64,7 @@ $(function () {
         var translation = 7.5 + ((index - 1) * 15);
         $("#selector").animate({left: translation +'%'});
         $("#documentation").find(".active").removeClass("active");
-        $(".documentationParagraph")[secondIndex].addClass("active");
+        console.log( $( ".documentationParagraph" ).get( -1 ) );
   });
 });
 
