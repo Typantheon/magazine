fbe8f731a2d50e173497af68318ef4499e1dddb8
Sebastiano-G <92300303+Sebastiano-G@users.noreply.github.com>
2022-09-13 12:15:30 +0200

    Update jsCode.js

--- jsCode.js
+++ jsCode.js
@@ -101,6 +101,7 @@ function highlight(entity) {
 
 $(function () {
     $('.badge').click(function () {
+        $("#badgeList").find(".activeBadge").removeClass(".activeBadge");
         this.classList.add('activeBadge');
         var index = $(this).parent().children().index(this);
         $("#entities").find(".active").removeClass("active");
