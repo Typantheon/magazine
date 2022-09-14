74ac58be2014e18583a60fd957e5192ec8ad41e0
Sebastiano-G <92300303+Sebastiano-G@users.noreply.github.com>
2022-09-14 23:19:45 +0200

    Update jsCode.js

--- jsCode.js
+++ jsCode.js
@@ -46,9 +46,12 @@ function changeStylesheet(css) {
     if (css != '../retro60s.css') {
         const imgs = document.getElementsByTagName('img');
         for (let i = 0; i < imgs.length; i++) {
-            if(imgs[i].getAttribute('id') == 'over-img-1' || imgs[i].getAttribute('id') == 'over-abstract') {
+            if(imgs[i].getAttribute('id') == 'over-img-1') {
                 imgs[i].remove();
             }
+            else if (imgs[i].getAttribute('id') == 'over-abstract') {
+                imgs[i].remove();
+            }    
         }
     }
     if (css != '../manga.css') {
